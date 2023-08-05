276de73e25880aa440c7c6a29e5eaf2a84fb5e3e
Lera <valeriyakiseleva-1221@mail.ru>
2023-08-05 22:53:12 +0300

    hw5

--- script.js
+++ script.js
@@ -70,3 +70,66 @@ console.log(user[info]);
 let userName = prompt('Как тебя зовут?');
 
 alert(`Привет, ${userName} !`);
+
+
+
+
+
+
+
+
+
+function min(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }  
+  }
+  function num (n) {
+    return n% 2 ==0;
+  }
+
+  function num (n) {
+    if (n% 2 == 0) {
+        alert('Четное')
+    } else {
+        alert('Нечетное')
+    }
+  }
+
+  function number (a) {
+    return a*a;
+  }
+
+  function checkAge (age) {
+    alert('Сколько тебе лет?')
+    if(age < 0) {
+        console.log('Вы ввели неправильное значение')
+    } else if (age <= 12) {
+        console.log ('Привет,друг!')
+    }else if (age >= 13) {
+        console.log ('Добро пожаловать!')
+    }
+  }
+
+  function numbers(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+      return 'Одно или оба значения не являются числом';
+    } else {
+      return a * b;
+    }
+  }
+
+  function cubeNumber() {
+    const user = prompt("Введите число:");
+    const number = Number(user);
+    if (isNaN(number)) {
+      return "Переданный параметр не является числом";
+    }
+    const cube = Math.pow(number, 3);
+    return `${number} в кубе равняется ${cube}`;
+  }
+  for (let i = 0; i <= 10; i++) {
+    console.log(cubeNumber(i));
+  }
